24c6aaaef87a0978d7490eb764026023eaf25eee
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-07-05 11:24:52 -0300

    Primer Commit
    Estos son los 6 ejercicios de la primera clase.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,20 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+  //var person = prompt("Please enter your name", "Harry Potter");
+  /*Esto es un comentario de bloque*/
+   
+   	var nombre;
+   	nombre= "Franco";
+   	//alert(nombre);
+   	nombre= prompt("Ingrese su nombre");
+   	alert("Su nombre es: " + nombre);
+
+
+
+
+
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var Num1;
+var Num2;
+var Suma;
 
+Num1= parseInt(document.getElementById('numeroUno').value);
+Num2= parseInt(document.getElementById('numeroDos').value);
+
+suma= num1 + num2;
+alert("La suma es " + suma);
 }
 
