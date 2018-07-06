b4561a2e65e55eb205fb6de9d2ed8e61545e7a24
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-07-06 12:02:32 -0300

    Ejercicios de parcial Practica 6/7

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+	var Precio;
+	var Descuento;
+	var IVA;
+	var PrecioconDescuento;
+	var DescuentoenDinero;
+	var PrecioFinal;
+	Precio= prompt("Ingrese el precio");
+	Descuento= prompt("Ingrese el descuento");
+
+	DescuentoenDinero= (Precio * Descuento) / 100;
+
+	PrecioconDescuento= Precio - DescuentoenDinero;
+
+	IVA= Precio * 0.21;
+
+	PrecioFinal= parseInt(PrecioconDescuento) + parseInt(IVA);
+	document.getElementById('elPrecioFinal').value= PrecioFinal;
+	
+
+	alert("El Descuento en dinero es " + DescuentoenDinero + " El precio con el descuento es " + PrecioconDescuento + " Y el IVA es " + IVA);
+
 
 }
