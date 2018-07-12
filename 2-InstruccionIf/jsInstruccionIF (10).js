e036e6f2a71216a2f1eb2c817ad0c2b97815b105
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-07-12 12:22:40 -0300

    ejercicios de random

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var numeroRandom;
+	numeroRandom=Math.floor(Math.random() * 10) + 1; 
+	if (numeroRandom >= 9) 
+	{
+		alert("Su nota es " + numeroRandom + ".Excelente");
+	}
+	else if (numeroRandom >=4 && numeroRandom < 9) 
+	{
+		alert("Su nota es " + numeroRandom + ".Aprobo");
+	}
+	else
+	{
+		alert("Su nota es" + numeroRandom + ".Vamos, la proxima se puede");
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,7 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-
+	var numeroRandom;
+	numeroRandom=Math.floor(Math.random() * 10) + 1; 
+	alert("El numero es " + numeroRandom);
 }//FIN DE LA FUNCIÓN
