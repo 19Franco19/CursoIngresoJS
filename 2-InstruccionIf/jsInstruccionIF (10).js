c93bcf6ef6e8bf30cbe4b3167f1555bbafa98c76
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-07-13 12:10:54 -0300

    clase 13/07

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -5,14 +5,14 @@ function mostrar()
 	numeroRandom=Math.floor(Math.random() * 10) + 1; 
 	if (numeroRandom >= 9) 
 	{
-		alert("Su nota es " + numeroRandom + ".Excelente");
-	}
+		alert("Su nota es " + numeroRandom +  ": Excelente");
+	} 
 	else if (numeroRandom >=4 && numeroRandom < 9) 
 	{
-		alert("Su nota es " + numeroRandom + ".Aprobo");
+		alert("Su nota es " + numeroRandom +  ": Aprobo");
 	}
 	else
 	{
-		alert("Su nota es" + numeroRandom + ".Vamos, la proxima se puede");
+		alert("Su nota es " + numeroRandom + " : Vamos, la proxima se puede");
 	}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,9 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
+	
+	switch(mesDelAño)
+{
+	
+	case"Febrero":
+		alert("Este mes no tiene mas de 29 dias");
+		break;
 
-//alert (mesDelAño);
+	default:
+		alert("Este mes tiene 30 o mas dias");
+
+	
+}
+	   
 	
 	
 
