330f249312f12489f18d96d20dc60700ebb62c5e
19Franco19 <36230567+19Franco19@users.noreply.github.com>
2018-07-27 12:07:07 -0300

    Ejercicio 7 de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,73 @@
+/*
+tomar datos
+
+
+1-
+2-
+3-
+*/
 function mostrar()
 {
+	// variables
+	var edades;
+	var sexo;
+	var nombre;
+
+
+
+	//inicializaciones
+	var contador=0
+	var edades=0
+	var respuesta="si";
+			//tomardatos
+			//pedir
+			//parsear
+			//validar
+
+	while(respuesta !="no")
+	{	
+		edades= prompt('Ingrese Una Edad:');
+		edades=parseInt(edades);
+		respuesta= prompt('Ingrese no para salir');
+
+		while(edades  > 0 && edades < 100 )
+		{
+			edades= prompt('Ingrese Una Edad:');
+			edades=parseInt(edades);
+		}
+
+		sexo= prompt('Ingrese El Sexo:');
+
+		while(sexo == "f" || sexo == "m")
+		
+		{
+		sexo= prompt('Ingrese El Sexo:');
+		}
+
+		nombre= prompt('"Ingrese El Nombre:');
+
+		while(nombre)
+		{
+		nombre= prompt('"Ingrese El Nombre:');
+	    }
+
+
+
+		
+
+
+
+		//analisis
+
+			
+
+	}
+
+	//mas operaciones
+
+
+
+	//mostrar
+
 
 }
